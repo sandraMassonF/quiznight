39cd57c13387d2450d6b04a5e691c1de139559ca
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-02-12 10:28:45 +0100

    delete absolute position for footer

--- component/footer.php
+++ component/footer.php
@@ -1,12 +1,10 @@
-<footer class="footer_container">
-    <div class="footer">
-        <h3>Join us now !</h3>
-        <div class="social-box">
-            <a href="#">Instagram</a>
-            <a href="#">Facebook</a>
-            <a href="#">Twitter</a>
-            <a href="#">TikTok</a>
-        </div>
+<footer class="footer">
+    <h3>Join us now !</h3>
+    <div class="social-box">
+        <a href="#">Instagram</a>
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">TikTok</a>
     </div>
 </footer>
 </body>
