18bfaec339e646a9959dac93e0cf14d5d666cb37
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-02-12 11:16:21 +0100

    delete getter setter class user on user.php

--- page/user.php
+++ page/user.php
@@ -3,46 +3,21 @@
 
 class Utilisateur{
 
+private $id;
 private $pseudo;
 private $password;
 private $score;
 
 
-public function __construct(string $pseudo, string $password, int $score) {
+public function __construct() {
 
-    $this->pseudo = $pseudo;
-    $this->password = $password;
-    $this->score = $score;
-}
-
-public function getPseudo()
-{
-return $this->pseudo;
-}
- 
-public function setPseudo($pseudo)
-{
-return $this->pseudo = $pseudo;
+    $this->id;
+    $this->pseudo;
+    $this->password;
+    $this->score;
 }
 
-public function getPassword()
-{
-return $this->password;
-}
 
-public function setPassword($password)
-{
-return $this->password = $password;
-}
-public function getScore()
-{
-return $this->score;
-}
-
-public function setScore($score)
-{
-return $this->score = $score;
-}
 
 
 
