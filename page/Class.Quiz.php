28538de998bc21c53fbf501181582d2b24d75369
Bossola-Yannick <bossolay@hotmail.fr>
2025-02-12 11:34:52 +0100

    feat: get all quiz on page autor

--- page/Class.Quiz.php
+++ page/Class.Quiz.php
@@ -3,12 +3,21 @@ include_once("./Class.Bdd.php");
 
 class Quiz
 {
-
     public function getAllQuiz()
     {
         $newBdd = new ConnexionBdd();
         $bdd = $newBdd->Connextion();
         $sql = "SELECT * FROM quiz";
+        $stmt = $bdd->prepare($sql);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function getQuizUser($id)
+    {
+        $newBdd = new ConnexionBdd();
+        $bdd = $newBdd->Connextion();
+        $sql = "SELECT * FROM quiz WHERE id_user";
         $stmt = $bdd->prepare($sql);
         $stmt->execute();
 
