d7828384e8e94c54db7ad9322c581f98cb57686b
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-02-13 15:28:56 +0100

    add 2 images in asset/img, add css in style_header-footer and  style_index

--- component/header.php
+++ component/header.php
@@ -2,8 +2,7 @@
 <html lang="fr">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../asset/css/header-footer.css">
-<link rel="stylesheet" href="./asset/css/header-footer.css">
+<link rel="stylesheet" href="../asset/css/style_header-footer.css">
 <link rel="stylesheet" href="../asset/css/style_connexion.css">
 <link rel="stylesheet" href="../asset/css/style_quiz-jeu.css">
 <link rel="stylesheet" href="../asset/css/style_autor-page.css">
